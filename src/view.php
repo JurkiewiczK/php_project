c6944a77d46fd37c2912c1a979857b1e21e1cde9
JurkiewiczK <kwjurkiewicz@gmail.com>
2022-12-09 07:08:26 +0100

    View refactor

--- src/view.php
+++ src/view.php
@@ -3,12 +3,29 @@ declare(strict_types=1);
 
 namespace App;
 
-class View 
+class View
 {
-    public function render(string $page, array $params): void
-    {
+  public function render(string $page, array $params = []): void
+  {
+    $params = $this->escData($params);
+    require_once("templates/layout.php");
+  }
+
+  private function escData(array $params): array
+  {
+    $valuesBox = [];
+
+    foreach ($params as $key => $value) {
+      if (is_array($value)) {
         
-        require_once("templates/layout.php");
+        $valuesBox[$key] = $this->escData($value);
+      } else if ($value) {
+        $valuesBox[$key] = htmlspecialchars(($value.''), ENT_QUOTES);
+      } else {
+        $valuesBox[$key] = $value;
+      }
     }
 
+    return $valuesBox;
+  }
 }
